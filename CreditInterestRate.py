daf55c246724b867463de3f2ae1f3449dbe696a1
Michael Mullet <Michaelmullet007@gmail.com>
2021-07-05 18:19:57 -0400

    Clean up code

--- CreditInterestRate.py
+++ CreditInterestRate.py
@@ -1,17 +1,17 @@
-balance = 3926
+balance = 5000
 annualInterestRate = 0.2
 
 newBalance = balance
-monthIntRate = (annualInterestRate)/12.0
+monthIntRate = (annualInterestRate) / 12.0
 epsilon = 0.01
-low = newBalance / 12
+low = newBalance / 12.0
 high = (newBalance * (1 + monthIntRate)**12) / 12.0
 monthlyPayment = (high + low) / 2.0
 
 while abs(high - low) >= epsilon:
     for month in range(12):
         newBalance -= monthlyPayment
-        newBalance += (annualInterestRate/12.0) * newBalance
+        newBalance += monthIntRate * newBalance
     if newBalance > 0:
         month = 0
         newBalance = balance
@@ -20,5 +20,4 @@ while abs(high - low) >= epsilon:
         month = 0
         newBalance = balance
         high = monthlyPayment
-    monthlyPayment = (high+low)/2.0
 print('Lowest Payment: ' + str(round(monthlyPayment, 2)))
